683bb746ed34435cba590eaebc94985468f3d138
Christopher Jones <tkcwebdev@gmail.com>
2022-05-05 01:55:43 -0700

    Initial Commit

---
+++ Shared/ito_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ito_mobileApp.swift
+//  Shared
+//
+//  Created by Christopher Jones on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ito_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
